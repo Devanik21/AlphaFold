a1c10c57908194e3e7575f884f502035c2c60e10
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-06-14 20:14:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 import pandas as pd
 import numpy as np
 import plotly.express as px
-import plotly.graph_objects as go
+import plotly.graph_objects as go;
 from plotly.subplots import make_subplots
 import random
 import string
